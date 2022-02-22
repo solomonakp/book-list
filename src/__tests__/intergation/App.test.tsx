04c4cfb9b9c53a0d3300cb9196415be21e9db65c
solomonakp <solomonakp@gmail.com>
2022-02-22 21:29:06 +0100

    added more test

--- src/__tests__/intergation/App.test.tsx
+++ src/__tests__/intergation/App.test.tsx
@@ -18,12 +18,12 @@ import App from 'App';
 // queries
 import { GET_BOOKS, GET_AUTHORS, ADD_AUTHOR } from 'apollo/queries';
 
-// beforeEach(() => {
-//   jest.useFakeTimers();
-// });
-// afterEach(() => {
-//   jest.clearAllMocks();
-// });
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+afterEach(() => {
+  jest.resetAllMocks();
+});
 
 const setUpApp = (
   mock: readonly MockedResponse<Record<string, any>>[] | undefined
@@ -85,96 +85,96 @@ describe('bookstore integrations tests', () => {
     expect(await screen.findByText(/Add a Author/i)).toBeInTheDocument();
   });
 
-  // it('should be able to add a book', async () => {
-  //   const [book1] = [BuildBook()];
+  it('should be able to add a book', async () => {
+    const [book1] = [BuildBook()];
 
-  //   const [author1, author2] = [BuildAuthor(), BuildAuthor()];
+    const [author1, author2] = [BuildAuthor(), BuildAuthor()];
 
-  //   const entry = {
-  //     ...book1,
-  //     authorId: author1.id,
-  //     author: {
-  //       name: author1.name,
-  //     },
-  //   };
+    const entry = {
+      ...book1,
+      authorId: author1.id,
+      author: {
+        name: author1.name,
+      },
+    };
 
-  //   const mocks = [
-  //     {
-  //       request: {
-  //         query: GET_AUTHORS,
-  //       },
-  //       result: {
-  //         data: {
-  //           authors: [
-  //             { id: author1.id, name: author1.name },
-  //             { id: author2.id, name: author2.name },
-  //           ],
-  //         },
-  //       },
-  //     },
-  //     {
-  //       request: {
-  //         query: ADD_AUTHOR,
-  //         // variables: {
-  //         //   title: entry.title,
-  //         //   authorId: author1.id,
-  //         //   genre: entry.genre,
-  //         // },
-  //       },
-  //       result: {
-  //         data: {
-  //           addBook: [{ title: entry.title, genre: entry.genre }],
-  //         },
-  //       },
-  //     },
-  //     {
-  //       request: {
-  //         query: GET_BOOKS,
-  //       },
-  //       result: {
-  //         data: {
-  //           books: [
-  //             {
-  //               authorId: entry.authorId,
-  //               genre: entry.genre,
-  //               id: entry.id,
-  //               title: entry.title,
-  //             },
-  //           ],
-  //         },
-  //       },
-  //     },
-  //   ];
+    const mocks = [
+      {
+        request: {
+          query: GET_AUTHORS,
+        },
+        result: {
+          data: {
+            authors: [
+              { id: author1.id, name: author1.name },
+              { id: author2.id, name: author2.name },
+            ],
+          },
+        },
+      },
+      {
+        request: {
+          query: ADD_AUTHOR,
+          // variables: {
+          //   title: entry.title,
+          //   authorId: author1.id,
+          //   genre: entry.genre,
+          // },
+        },
+        result: {
+          data: {
+            addBook: [{ title: entry.title, genre: entry.genre }],
+          },
+        },
+      },
+      {
+        request: {
+          query: GET_BOOKS,
+        },
+        result: {
+          data: {
+            books: [
+              {
+                authorId: entry.authorId,
+                genre: entry.genre,
+                id: entry.id,
+                title: entry.title,
+              },
+            ],
+          },
+        },
+      },
+    ];
 
-  //   setUpApp(mocks);
+    setUpApp(mocks);
 
-  //   const titleInput = await screen.findByLabelText(/title/i);
-  //   const genreInput = await screen.findByLabelText(/genre/i);
-  //   const option = (await screen.findByText(author1.name)) as HTMLOptionElement;
-  //   const addBtn = await screen.findByRole('button');
+    const titleInput = await screen.findByLabelText(/title/i);
+    const genreInput = await screen.findByLabelText(/genre/i);
+    const option = (await screen.findByText(author1.name)) as HTMLOptionElement;
+    const addBtn = await screen.findByRole('button');
 
-  //   expect(addBtn).toBeDisabled();
+    expect(addBtn).toBeDisabled();
 
-  //   userEvent.type(titleInput, entry.title);
-  //   userEvent.type(genreInput, entry.genre);
-  //   userEvent.selectOptions(await screen.findByRole('combobox'), option);
+    userEvent.type(titleInput, entry.title);
+    userEvent.type(genreInput, entry.genre);
+    userEvent.selectOptions(await screen.findByRole('combobox'), option);
 
-  //   await waitFor(() => {
-  //     expect(option.selected).toBe(true);
-  //   });
+    await waitFor(() => {
+      expect(option.selected).toBe(true);
+    });
 
-  //   expect(addBtn).toBeEnabled();
+    expect(addBtn).toBeEnabled();
 
-  //   userEvent.click(addBtn);
+    userEvent.click(addBtn);
 
-  //   await waitFor(() => {
-  //     expect(screen.getByText(/error/i)).toBeInTheDocument();
-  //   });
+    await waitFor(() => {
+      expect(screen.getByText(/error/i)).toBeInTheDocument();
+    });
 
-  //   // screen.debug(await screen.findByText();
+    // screen.debug(await screen.findByText();
 
-  //   // expect().toBeInTheDocument();
-  // });
+    // expect().toBeInTheDocument();
+  });
 
   it('should be able to add to  author', () => {});
   it('should be able to display errors', () => {});
